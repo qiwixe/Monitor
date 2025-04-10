9fad6c7112b6cc3fef0086f0f6c67a91fc673d8a
qiwixe <Qiwixe@gmail.com>
2025-04-10 21:54:48 +0500

    revert

---
+++ hybr.Shared/Services/GraduationSettings.cs
@@ -0,0 +1,16 @@
+﻿using BlazorBootstrap;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hybr.Shared.Services
+{
+    internal class GraduationSettings
+    {
+
+
+    }
+}
---
+++ hybr.Shared/Services/UpdateGrid.cs
@@ -0,0 +1,20 @@
+﻿using BlazorBootstrap;
+
+namespace hybr.Shared.Services
+{
+    public class UpdateGrid
+    {
+        public static Dictionary<string, Grid<Sensor>> AllUpdateGrid { get; set; } =
+        new Dictionary<string, Grid<Sensor>>
+        {
+            ["Home"] = new(),
+            ["Photovoltaic"] = new(),
+            ["WindPower"] = new(),
+            ["SolarCollector"] = new(),
+            ["SolarConcentrator"] = new(),
+            ["HeatPump"] = new(),
+            ["Bioplant"] = new(),
+            ["Meteorological"] = new()
+        };
+    }
+}
