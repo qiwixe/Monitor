3fbd3ed8b4452fd1ef51c136e95bae4d932a69fc
qiwixe <Qiwixe@gmail.com>
2025-04-10 21:41:50 +0500

    del for perf

--- hybr.Shared/Services/IFormFactor.cs
+++
@@ -1,8 +0,0 @@
-namespace hybr.Shared.Services
-{
-    public interface IFormFactor
-    {
-        public string GetFormFactor();
-        public string GetPlatform();
-    }
-}
---
+++ hybr.Shared/Services/LiveChartOptions.cs
@@ -0,0 +1,14 @@
+﻿using BlazorBootstrap;
+
+namespace hybr.Shared.Services
+{
+    internal class LiveChartOptions : LineChartOptions
+    {
+        public bool Animation { get; set; } = false;
+        public new string? IndexAxis { get; set; } = new("x");
+        public new Interaction Interaction { get; set; } = new Interaction { Mode = InteractionMode.Index, Intersect = false };
+        public new bool Responsive { get; set; } = true;
+        //public new Scales Scales { get; set; } = new Scales() { Y = new ChartAxes() { Min = 100, Max = 300 } };
+    }
+
+}
